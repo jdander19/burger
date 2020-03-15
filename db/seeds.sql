8a0b8448b4eb4747538e54be8e0e6c2ba6e541f9
Jesse Anderson <lockbox20@gmail.com>
2020-03-14 21:17:56 -0500

    new files

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (id, burger_name, devoured, date)
+VALUES
+(null, "Cheesey Cheese Burger", FALSE, null),
+(null, "Quarter Pounder Royal Burger", FALSE, null),
+(null, "Mega 1 Pounder Burger", FALSE, null),
+(null, "Bacon Anhillation Burger", FALSE, null),
+(null, "Quadruple Triple Double Decker Burger", FALSE, null),
+(null, "Flaming Burning Buffalo Burger", FALSE, null);
